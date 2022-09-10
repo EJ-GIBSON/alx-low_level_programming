c47801febffdb31af8ce6d2a926c3174a2cebad8
EJ-GIBSON <ejidechukwu@gmail.com>
2022-09-10 07:58:08 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	       putchar((num % 10) + '0');
 
-	for (x = 'a'; x <= 'f'; letter++)
+	for (x = 'a'; x <= 'f'; x++)
 		putchar(x);
 
 	putchar('\n');
