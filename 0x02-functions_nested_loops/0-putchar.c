75be2b99267deb8e3fd8bbfa4964ef7510495a8f
EJ-GIBSON <ejidechukwu@gmail.com>
2022-09-14 06:22:52 -0700

    Printed putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * main - prints putchar
  * Return: always 0
  */
@@ -11,9 +11,9 @@ int main(void)
 	_putchar('u');
 	_putchar('t'); 
 	_putchar('c');
-       	_putchar('h');
+	_putchar('h');
 	_putchar('a');
-     	_putchar('r');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
